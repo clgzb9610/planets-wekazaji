6cd8f09312a341bb05546174c0594f1978a66649
kaylabeckm <kaylabeckm@gmail.com>
2017-10-17 16:28:00 -0500

    Added Asset Directory Replaced crates with the player character. Implemented animations. I added in a camera follow feature to see what it does, but I haven't played with it yet.

--- game.js
+++ game.js
@@ -13,16 +13,18 @@ var crateGroup;
 var planetGroup;
 var cursors;
 
-// a force reducer to let the simulation run smoothly
+var player;
+var walkR;
+var walkL;
 
-var forceReducer = 0.005;
+// a force reducer to let the simulation run smoothly
+var forceReducer = 0.01; //was .005
 
 // graphic object where to draw planet gravity area
-
 var gravityGraphics;
 
 window.onload = function(){
-    game = new Phaser.Game(800, 600, Phaser.AUTO, "");
+    game = new Phaser.Game(900, 800, Phaser.AUTO, "");
     game.state.add("PlayGame",playGame);
     game.state.start("PlayGame");
 };
@@ -31,10 +33,11 @@ var playGame = function(game){};
 
 playGame.prototype = {
     preload: function () {
-        game.load.image("crate", "crate.png");
-        game.load.image("planet", "planet.png");
-        game.load.image("bigplanet", "bigplanet.png");
-        game.load.image('dude', 'dude.png');
+        game.load.image("crate", "assets/crate.png");
+        game.load.image("planet", "assets/planet.png");
+        game.load.image("bigplanet", "assets/bigplanet.png");
+        game.load.image('dude', 'assets/dude.png');
+        game.load.spritesheet('player',"assets/nebsprites.png",40,50);
     },
     create: function () {
 
@@ -44,7 +47,6 @@ playGame.prototype = {
         planetGroup = game.add.group();
 
         // adding graphic objects
-
         gravityGraphics = game.add.graphics(0, 0);
         gravityGraphics.lineStyle(2, 0xffffff, 0.5);
 
@@ -54,20 +56,21 @@ playGame.prototype = {
         // physics initialization
         game.physics.startSystem(Phaser.Physics.BOX2D);
 
-        // adding a couple of planets. Arguments are:
-        // x position
-        // y position
-        // gravity radius
-        // gravity force
-        // graphic asset
-        // addPlanet(180, 200, 250, 150, "planet");
-        addPlanet(400, 300, 400, 250, "bigplanet");
+        /* adding a couple of planets. Arguments are:
+         * x position, y position, gravity radius, gravity force, graphic asset */
+        addPlanet(180, 200, 250, 150, "planet");
+        addPlanet(600, 400, 400, 250, "bigplanet");
 
         // waiting for player input
-        game.input.onDown.add(addCrate, this);
+        // game.input.onDown.add(addCrate, this);
+        player = game.add.sprite(650, 100, "player");
+        player.frame = 4;
+        walkR = player.animations.add('walkR',[5,6,7,8], 15, true);
+        walkL = player.animations.add('walkL', [0,1,2,3], 15, true);
+        stand = player.animations.add('stand',[4],1);
 
         // player = game.add.sprite(200, 200, 'dude');
-        // game.physics.box2d.enable(player);
+        game.physics.box2d.enable(player);
         // player.body.fixedRotation = true;
 
         cursors = game.input.keyboard.createCursorKeys();
@@ -75,57 +78,65 @@ playGame.prototype = {
 
     update: function(){
 
-        // looping through all crates
-        for (var i = 0; i < crateGroup.total; i++) {
-            var c = crateGroup.getChildAt(i);
+        // looping through all planets
+        for (var j = 0; j < planetGroup.total; j++) {
+            var p = planetGroup.getChildAt(j);
 
-            // looping through all planets
-            for (var j = 0; j < planetGroup.total; j++) {
-                var p = planetGroup.getChildAt(j);
+            // calculating distance between the planet and the crate
+            var distance = Phaser.Math.distance(player.x, player.y, p.x, p.y);
 
-                // calculating distance between the planet and the crate
-                var distance = Phaser.Math.distance(c.x, c.y, p.x, p.y);
+            // checking if the distance is less than gravity radius
+            if (distance < p.width / 2 + p.gravityRadius / 2) {
 
-                // checking if the distance is less than gravity radius
-                if (distance < p.width / 2 + p.gravityRadius / 2) {
+                // calculating angle between the planet and the crate
+                var angle = Phaser.Math.angleBetween(player.x, player.y, p.x, p.y);
 
-                    // calculating angle between the planet and the crate
-                    var angle = Phaser.Math.angleBetween(c.x, c.y, p.x, p.y);
+                // add gravity force to the crate in the direction of planet center
+                player.body.applyForce(p.gravityForce * Math.cos(angle) * forceReducer,
+                                  p.gravityForce * Math.sin(angle) * forceReducer);
 
-                    // add gravity force to the crate in the direction of planet center
-                    c.body.applyForce(p.gravityForce * Math.cos(angle) * forceReducer,
-                                      p.gravityForce * Math.sin(angle) * forceReducer);
-
-                }
             }
+         }
 
-            //Handle keyboard input for the crates
-            if (cursors.left.isDown) {
-                c.body.moveLeft(90);
-            }
-            else if (cursors.right.isDown) {
-                c.body.moveRight(90);
-            }
-            if (cursors.up.isDown) {
-                c.body.moveUp(90);
-            }
-            else if (cursors.down.isDown) {
-                c.body.moveDown(90);
-            }
+        //Handle keyboard input for the player
+        if (cursors.left.isDown) {
+            player.body.moveLeft(90);
+            player.animations.play('walkL');
+        }
+        else if (cursors.right.isDown) {
+            player.body.moveRight(90);
+            player.animations.play('walkR');
+        }
+        if (cursors.up.isDown) {
+            player.body.moveUp(90);
+            player.animations.play('stand');
+        }
+        else if (cursors.down.isDown) {
+            player.body.moveDown(90);
+            player.animations.play('stand');
+        }
+
+        if (cursors.left.justUp || cursors.right.justUp){
+            player.animations.play('stand');
         }
 
 
+    },
+
+    render: function() {
+        game.debug.cameraInfo(game.camera, 32, 32);
+        game.debug.spriteCoords(player, 32, 500);
 
     }
+
 };
 
 // function to add a crate
-
-function addCrate(e){
-    var crateSprite = game.add.sprite(e.x, e.y, "crate");
-    crateGroup.add(crateSprite);
-    game.physics.box2d.enable(crateSprite);
-}
+// function addCrate(e){
+//     var crateSprite = game.add.sprite(e.x, e.y, "crate");
+//     crateGroup.add(crateSprite);
+//     game.physics.box2d.enable(crateSprite);
+// }
 
 // function to add a planet
 function addPlanet(posX, posY, gravityRadius, gravityForce, asset){

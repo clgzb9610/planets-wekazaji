8732de8c7d52442b34124088b72ace45f8268507
kaylabeckm <kaylabeckm@gmail.com>
2017-11-15 10:23:47 -0600

    World rotates gradually when you change gravity fields. Some renaming / refactoring.

--- src/game.js
+++ src/game.js
@@ -36,7 +36,7 @@ var planetContact = false;
 // graphic object where to draw planet gravity area
 var gravityGraphics;
 
-var currentLevel = 1;
+var currentLevel = 0;
 /* x position, y position, gravity radius, gravity force, graphic asset */
 var level = [
     [//level 0 - tutorial, jumping between planets
@@ -55,7 +55,7 @@ var level = [
         {objectType: 'teleporter', x:248, y: 140, radians: 1.48, goal: 1}, //temporary change in coordinate, was y=31
         {objectType: 'gear', x: 100, y: -50, sprite: "gear"},
         {objectType: 'gear', x: -200, y: -150, sprite: "gear"},
-        // {objectType: 'player', x: 25, y: 205}
+        {objectType: 'player', x: 25, y: 205}
     ],
     [//level 2 - jumping to planets through void
         {objectType:"level3"},
@@ -107,7 +107,7 @@ playGame.prototype = {
         game.physics.startSystem(Phaser.Physics.BOX2D);
 
 
-        drawLevel();
+        createLevel();
 
 
         // waiting for player input
@@ -141,29 +141,15 @@ playGame.prototype = {
         game.camera.follow(player);
     },
     update: function(){
-        // console.log('planet contact', planetContact);
+        var playerAngle = handleRotation(player);
 
-        var angle = gravityToPlanets(player);
-        if (angle > -361){ // angle == -361 if the player is not in any gravity field.
-            //orients players feet toward the ground. Uses var angle as degrees offset by -90
-            player.body.angle = angle * 180 / Math.PI - 90;
-            lastAngle = angle;
-        } else{
-            angle = lastAngle;
-        }
-
-        objGrav();
+        applyGravityToObjects();
         checkTeleporterOverlap(teleporter);
 
-        game.world.pivot.x = player.x;          //these two rotate the world around the player
-        game.world.pivot.y = player.y;
-        game.world.rotation = -angle + (Math.PI/2);     //rotates the world so the controls aren't global
-
         //Handle keyboard input for the player
-        keyboardInput(angle);
+        handleKeyboardInput(playerAngle);
 
         constrainVelocity(player,150);
-
     },
     render: function() {
         game.debug.cameraInfo(game.camera, 32, 32);
@@ -181,7 +167,7 @@ playGame.prototype = {
 //     game.physics.box2d.enable(crateSprite);
 // }
 
-function drawLevel(){
+function createLevel(){
     for (var i = 0; i < level[currentLevel].length; i++) {
         var addition = level[currentLevel][i];
         if (addition.objectType === 'planet') {
@@ -200,6 +186,57 @@ function drawLevel(){
 
     }
 }
+/* calculates angle between the player and the planet it is gravitationally attracted to.
+ * Orients the player's feet to the ground & handles all the world rotation.
+ */
+function handleRotation(player){
+    var angleToPlanet = gravityToPlanets(player);
+    var playerAngle;
+
+    if (lastAngle === undefined){
+        playerAngle = angleToPlanet;
+        lastAngle = playerAngle;
+    }
+    if (angleToPlanet > -361){ // angle == -361 if the player is not in any gravity field.
+        //orients players feet toward the ground. Uses var angle as degrees offset by -90
+        var playerRotationToGravity = radiansDelta(lastAngle, angleToPlanet);
+        var maxPlayerRotationSpeed = 0.15;
+
+        if (Math.abs(playerRotationToGravity) <= maxPlayerRotationSpeed){
+            playerAngle = angleToPlanet;
+        } else if ( playerRotationToGravity > Math.PI){
+            playerAngle = lastAngle + maxPlayerRotationSpeed;
+        } else {
+            playerAngle = lastAngle - maxPlayerRotationSpeed;
+        }
+
+        player.body.angle = angleToPlanet * 180 / Math.PI - 90;
+        lastAngle = playerAngle;
+    } else{
+        playerAngle = lastAngle;
+    }
+
+    game.world.pivot.x = player.x;          //these two rotate the world around the player
+    game.world.pivot.y = player.y;
+    game.world.rotation = -playerAngle + (Math.PI/2);     //rotates the world so the controls aren't global
+
+    return playerAngle;
+}
+
+/* Shortest distance between two angles in range -pi to pi.
+ *
+ */
+function radiansDelta(fromAngle, toAngle){
+    return normalizedRadians(fromAngle - toAngle + Math.PI) - Math.PI;
+}
+
+/* normalizes a angle to the range 0 to 2 pi.
+ *
+ */
+function normalizedRadians(rawAngle){
+    var TAU = Math.PI * 2;
+    return ((rawAngle % TAU) + TAU) % TAU;
+}
 
 /*
 This is the code that calculates gravity fields for the player, if they are in the radius.
@@ -207,34 +244,50 @@ I changed it so that this function returns -361, which is impossible in radian a
 if the player is not in any gravity radius.
  */
 function gravityToPlanets(gravObject){
-    var p;
-    var distance;
-    var radius;
-   var angle = -361;
+    var angle = -361;
+    var closestPlanetDistance = Infinity;
     // looping through all planets
-    for (var j = 0; j < planetGroup.total; j++) {
-        p = planetGroup.getChildAt(j);
-        radius = p.width / 2 + p.gravityRadius / 2;
+    for(var j = 0; j < planetGroup.total; j++){
+        var p = planetGroup.getChildAt(j);
+        var planetGravityRadius = p.width / 2 + p.gravityRadius / 2;
+        var distanceFromPlanet = Phaser.Math.distance(gravObject.x, gravObject.y, p.x, p.y);
 
-        // calculating distance between the planet and the crate
-        distance = Phaser.Math.distance(gravObject.x, gravObject.y, p.x, p.y);
-
-        // checking if the distance is less than gravity radius
-        if (distance < radius) {
-
+        if (distanceFromPlanet < planetGravityRadius){
             // calculating angle between the planet and the crate
             angle = Phaser.Math.angleBetween(gravObject.x, gravObject.y, p.x, p.y);
             // add gravity force to the crate in the direction of planet center
 
             // console.log(distance-p.width/2, radius-p.width);
-            gravObject.body.applyForce(p.gravityForce * Math.cos(angle) * forceReducer * (distance-p.width/2),
-                p.gravityForce * Math.sin(angle) * forceReducer* (distance-p.width/2));
+            gravObject.body.applyForce(p.gravityForce * Math.cos(angle) * forceReducer * (distanceFromPlanet-p.width/2),
+                p.gravityForce * Math.sin(angle) * forceReducer* (distanceFromPlanet-p.width/2));
         }
     }
     return angle;
 }
 
-function objGrav(){
+// function findClosestPlanet(gravObject){
+//     var closestPlanetDistance = Infinity;
+//     var closestPlanet;
+//
+//     for(var j = 0; j < planetGroup.total; j++){
+//         var p = planetGroup.getChildAt(j);
+//         var planetGravityRadius = p.width / 2 + p.gravityRadius / 2;
+//         var distanceFromPlanet = Phaser.Math.distance(gravObject.x, gravObject.y, p.x, p.y);
+//
+//         if (distanceFromPlanet < planetGravityRadius){
+//             if (closestPlanet === undefined){
+//                 closestPlanet = p;
+//                 closestPlanetDistance = distanceFromPlanet;
+//             } else if( distanceFromPlanet < closestPlanetDistance){
+//                 closestPlanet = p;
+//                 closestPlanetDistance = distanceFromPlanet;
+//             }
+//         }
+//     }
+//     return [closestPlanet,distanceFromPlanet];
+// }
+
+function applyGravityToObjects(){
     for (var i = 0; i < objectGroup.total; i++){
         var o = objectGroup.getChildAt(i);
         gravityToPlanets(o);
@@ -369,11 +422,11 @@ function changeLevel(){
 
     console.log('destroy!');
     score = 0;
-    drawLevel()
+    createLevel()
     // game.state.start("PlayGame", true, false, this.currentLevel);
 }
 
-function keyboardInput(angle){
+function handleKeyboardInput(angle){
     if (cursors.left.isDown ) {
         // player.body.moveLeft(90);
         player.body.velocity.x += vel * Math.cos(angle + (Math.PI/2));

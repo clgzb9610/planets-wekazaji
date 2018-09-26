1ee28f0d86996b615c29cca59a2292d32ff599e8
clgzb9610 <wkim@macalester.edu>
2018-09-26 16:29:25 -0500

    added jetpack to player sprite

--- src/game.js
+++ src/game.js
@@ -257,7 +257,7 @@ playGame.prototype = {
         game.load.image("gearPlanet", "assets/planets/sample_Planet_mid.png");
 
         game.load.image("space", "assets/game/seamlessspacebright.png");
-        game.load.spritesheet('player',"assets/game/nebspritesv2.5.png",40,47);
+        game.load.spritesheet('player',"assets/game/nebspritesv3.0.png",40,47);
         game.load.spritesheet("enemy", "assets/game/enemyblink.png", 41,43);
         game.load.spritesheet('gear', 'assets/game/gearspritessmall.png',38,34);
         game.load.spritesheet('teleporter', 'assets/game/teleporterspritesheet.png', 48, 61);

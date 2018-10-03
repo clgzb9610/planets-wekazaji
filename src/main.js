f141c0f6136f0cea3b47c10b9f74e61c0eb97143
erich1510 <ehuang@macalester.edu>
2018-10-03 15:52:02 -0500

    Added in toggles and lines for activating FPS counter

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 var main = function(game) {};
 
-var game = new Phaser.Game(700, 700, Phaser.AUTO, "");
+var game = new Phaser.Game(700, 700, Phaser.AUTO, 'Deep Space Diver', {create: create, render: render});
 
 game.state.add("MainMenu", mainMenu);
 game.state.add("PlayGame", playGame);
@@ -8,3 +8,11 @@ game.state.add("Ending", ending);
 game.state.add("levelSelect", levelSelect);
 
 game.state.start("MainMenu");
+
+function create() {
+    game.time.advancedTiming = true;
+
+}
+function render() {
+    game.debug.text(game.time.fps, 2, 14, "#00ff00");
+}
